17410ace301e8bcf5c4b2dbfb3198cf9160c4970
firhanafahiyah <114464228+firhanafahiyah@users.noreply.github.com>
2023-03-06 19:49:51 +0700

    PRAKTIKUM 2

---
+++ PRAKTIKUM2/proses_nilai.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_GET['submit'])){
+$nama = $_GET['nama'];
+$matkul = $_GET['matkul'];
+$uts = $_GET['uts'];
+$uas = $_GET['uas'];
+$praktikum = $_GET['praktikum'];
+
+echo "nama: $nama <br>";
+echo "mata kuliah: $matkul <br>";
+echo "nilai uts: $uts <br>";
+echo "nilai uas: $uas <br>";
+echo "nilai praktikum: $praktikum <br>";
+
+echo "<br> <br>";
+
+echo "nilai rata-rata"
+
+} else{
+    echo "data tidak ada";
+}
